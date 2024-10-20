f139184c2ab8a2762d06ab2d16c3f8e76a7bed4d
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2024-10-20 13:31:30 +0200

    Fix local dev env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/login-and-cart/",
+  base: process.env.NODE_ENV === "production" ? "/login-and-cart/" : "/",
   server: {
     host: "0.0.0.0",
     port: 3000,
